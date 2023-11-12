ae30980c3695fc6899647fc465e1ef605d9c5cc8
artisademi <artis.ademi@gmail.com>
2023-11-12 22:53:03 +0100

    Create User Endpoint

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/artisademi/gorm-fiber-api/database"
+	"github.com/artisademi/gorm-fiber-api/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,11 +12,19 @@ func welcome(c *fiber.Ctx) error {
 	return c.SendString("Welcome to this awesome api")
 }
 
+func setUpRoutes(app *fiber.App) {
+	// welcome endpoint
+	app.Get("/api", welcome)
+
+	// User Endpoint
+	app.Post("/api/users", routes.CreateUser)
+}
+
 func main() {
 	database.ConnectDb()
 	app := fiber.New()
 
-	app.Get("/api", welcome)
+	setUpRoutes(app)
 
 	log.Fatal(app.Listen(":8080"))
 }
